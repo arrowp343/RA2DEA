db05458d2f48726e6a9e7c4f266ad12b724d1214
arrowp343 <67070734+arrowp343@users.noreply.github.com>
2021-02-19 07:15:31 +0100

    implement first visitor

--- src/main/java/SyntaxTreeEvaluator.java
+++ src/main/java/SyntaxTreeEvaluator.java
@@ -1,12 +1,55 @@
 public class SyntaxTreeEvaluator implements Visitor{
-    // TODO erster Visitor: Denis
     public void visit(OperandNode node){
-
+        if(node.symbol==null){
+            node.nullable=true;
+        }
+        else{
+            node.nullable=false;
+            node.firstpos.add(node.position);
+            node.lastpos.add(node.position);
+        }
     }
     public void visit(BinOpNode node) {
-
+        if(node.operator.equals("|")){
+            node.nullable=((SyntaxNode)node.left).nullable | ((SyntaxNode)node.right).nullable;
+            node.firstpos.addAll(((SyntaxNode)node.left).firstpos);
+            node.firstpos.addAll(((SyntaxNode)node.right).firstpos);
+            node.lastpos.addAll(((SyntaxNode)node.left).lastpos);
+            node.lastpos.addAll(((SyntaxNode)node.right).lastpos);
+        }
+        if(node.operator.equals("°")){
+            node.nullable=((SyntaxNode)node.left).nullable & ((SyntaxNode)node.right).nullable;
+            if (((SyntaxNode)node.left).nullable){
+                node.firstpos.addAll(((SyntaxNode)node.left).firstpos);
+                node.firstpos.addAll(((SyntaxNode)node.right).firstpos);
+            }
+            else{
+                node.firstpos.addAll(((SyntaxNode)node.left).firstpos);
+            }
+            if(((SyntaxNode)node.right).nullable){
+                node.lastpos.addAll(((SyntaxNode)node.left).lastpos);
+                node.lastpos.addAll(((SyntaxNode)node.right).lastpos);
+            }
+            else{
+                node.lastpos.addAll(((SyntaxNode)node.right).lastpos);
+            }
+        }
     }
     public void visit(UnaryOpNode node){
-
+        if(node.operator.equals("*")){
+            node.nullable=true;
+            node.firstpos.addAll(((SyntaxNode)node.subNode).firstpos);
+            node.lastpos.addAll(((SyntaxNode)node.subNode).lastpos);
+        }
+        if(node.operator.equals("+")){
+            node.nullable=((SyntaxNode)node.subNode).nullable;
+            node.firstpos.addAll(((SyntaxNode)node.subNode).firstpos);
+            node.lastpos.addAll(((SyntaxNode)node.subNode).lastpos);
+        }
+        if(node.operator.equals("?")){
+            node.nullable=true;
+            node.firstpos.addAll(((SyntaxNode)node.subNode).firstpos);
+            node.lastpos.addAll(((SyntaxNode)node.subNode).lastpos);
+        }
     }
 }
